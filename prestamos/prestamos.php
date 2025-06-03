a24254c8bea09bec8e1235b123baee0282f042f6
Teo Fernández <81999491+TeoFernandez@users.noreply.github.com>
2025-06-03 15:35:12 -0300

    Añade la página de préstamos con opciones para gestionar préstamos de libros y productos.

---
+++ prestamos/prestamos.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Prestamos</title>
+</head>
+<body>
+    <h1>Prestamos</h1>
+    <p>Bienvenido a la sección de préstamos</p>
+
+    <p>En esta sección podrás gestionar los préstamos de libros y productos.</p>
+    <p>Selecciona una de las siguientes opciones:</p>
+    
+    <p><a href="">Solicitud de nuevo Prestamo</a></p>
+    <p><a href="">Devolucion de prestamo</a></p>
+    <p><a href="">Pendientes</a></p>
+    <p><a href="">Historial</a></p>
+    
+    
+    <p><a href="index.php">Volver</a></p>
+
+
+</body>
+</html>
