a0688b9be1b295f4df4639084214c95207f86959
Teo Fernández <81999491+TeoFernandez@users.noreply.github.com>
2025-06-08 18:12:34 -0300

    Añade el archivo de conexión a la base de datos bdconect.php

---
+++ bdconect.php
@@ -0,0 +1,16 @@
+<?php
+
+$host = 'localhost';
+$user = 'root';
+$pass = '';
+$dbname = 'bdbibloteca';
+$dbname = 'bdbibloteca';
+
+// Conexión con el puerto incluido
+$conn = new mysqli($host, $user, $pass, $dbname);
+
+// Verificar la conexión
+if ($conn->connect_error) {
+    die("Conexión fallida: " . $conn->connect_error);
+}
+?>
