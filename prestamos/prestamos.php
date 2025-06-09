2a41a218ea04687f60f47d51a26c7a15bfc75a22
Teo Fernández <81999491+TeoFernandez@users.noreply.github.com>
2025-06-08 21:03:33 -0300

    Añade archivo historialPrestamos.php y actualiza enlaces en la sección de préstamos a botones.

--- prestamos/prestamos.php
+++ prestamos/prestamos.php
@@ -12,12 +12,12 @@
     <p>En esta sección podrás gestionar los préstamos de libros y productos.</p>
     <p>Selecciona una de las siguientes opciones:</p>
     
-    <p><a href="newPrestamo.php">Solicitud de nuevo Prestamo</a></p>
-    <p><a href="devolucionDePrestamo.php">Devolucion de prestamo</a></p>
-    <p><a href="">Pendientes</a></p>
     <p><a href="">Historial</a></p>
-    
-    
+
+    <button><a href="newPrestamo.php">Solicitud de nuevo Prestamo</a></button>
+    <button><a href="devolucionDePrestamo.php">Devolucion de prestamo</a></button>
+    <button><a href="historialPrestamos.php">Historial</a></button>
+
     <p><a href="../index.php">Volver</a></p>
 
 
