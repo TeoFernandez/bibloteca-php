2f389438e3af900f66d4c566eece66e2a5b3c2a1
Teo Fernández <81999491+TeoFernandez@users.noreply.github.com>
2025-06-08 21:47:03 -0300

    Corrige el título de la página y mejora la presentación de la sección de artículos añadiendo encabezados y descripciones.

--- articulos/articulos.php
+++ articulos/articulos.php
@@ -3,9 +3,19 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>D</title>
+    <title>Articulos</title>
 </head>
 <body>
     
+    <h1>Articulos</h1>
+    <p>Bienvenido a la sección de artículos. Aquí encontrarás las funciones para la manipulación de articulos.</p>
+
+    <h2>Funciones disponibles:</h2>
+
+    <button><a href="ingresarArticulos.php">Ingresar Nuevos Articulos</a></button>
+    <button><a href="editarArticulos.php">Editar Articulos</a></button>
+    <button><a href="eliminarArticulos.php">Eliminar Articulos</a></button>
+    <br><br><br>
+    <button><a href="../index.php">Volver</a></button>
 </body>
 </html>
