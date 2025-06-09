593cb6875cba60151e3f3ebed496ab7003beeba9
Teo Fernández <81999491+TeoFernandez@users.noreply.github.com>
2025-06-08 23:21:34 -0300

    Corrige el título y mejora la presentación del formulario de eliminación de libros.

--- libros/eliminarLibros.php
+++ libros/eliminarLibros.php
@@ -3,9 +3,22 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Eliminar Libros</title>
 </head>
 <body>
     
+    <center>
+        <h1>Eliminar Libros</h1>
+    </center>
+    <p>En esta sección podrás eliminar libros del sistema.</p>
+    <h2>Formulario de Eliminación de Libros:</h2>
+    <form action="procesarEliminacion.php" method="post">
+        <label for="isbn">ISBN del Libro a Eliminar:</label>
+        <input type="text" id="isbn" name="isbn" required><br><br>
+        
+        <button type="submit">Eliminar Libro</button>
+        <button type="reset">Limpiar Formulario</button>
+        <br><br><br>
+        <button><a href="libros.php">Volver a la Sección de Libros</a></button>
 </body>
 </html>
