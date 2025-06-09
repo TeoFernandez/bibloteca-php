2ce8e9a943fdcf2b43807faecfaebf7d7bea7771
Nicolas Garcia <nicolasagustingarcia46@gmail.com>
2025-06-08 21:16:51 -0300

    Añade formularios para crear, editar y eliminar alumnos en el sistema.

---
+++ alumnos/crearAlumno.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Crear alumno</title>
+</head>
+<body>
+    <h1>Crear alumno</h1>
+    <p>Bienvenido a la seccion de creacion de alumno</p>
+    <center>
+        <form action="POST">
+            DNI del alumno: <input type="number" name="dni_alumno"><br>
+            Nombre del alumno: <input type="text" name="nombre_alumno"><br>
+            Apellido del alumno: <input type="text" name="apellido_alumno"><br>
+            Carrera del alumno: <input type="number" name="carrera_usuario"><br>
+            <input type="submit" value="Crear Alumno">
+        </form>
+    </center>
+
+    
+</body>
+</html>
---
+++ alumnos/eliminarAlumno.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Eliminar alumno</title>
+</head>
+<body>
+    <h1>Eliminar alumno</h1>
+    <p>Bienvenido a la seccion de eliminar alumno</p>
+
+    <center>
+        <form action="POST">
+            DNI del alumno: <input type="number" name="eliminar_usuario"><br>
+            <input type="submit" value="Eliminar Alumno">
+        </form>
+    </center>
+    
+</body>
+</html>
