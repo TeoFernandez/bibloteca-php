9d993457b0df3618b752ccbecd06d1ea72686c94
Nicolas Garcia <nicolasagustingarcia46@gmail.com>
2025-06-08 21:30:10 -0300

    Añade archivos para la gestión de usuarios: crear, editar, eliminar y listar usuarios con estructura HTML básica.

---
+++ usuarios/usuarios.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Usuarios</title>
+</head>
+<body>
+    <h1>Usuarios</h1>
+    <p>Bienvenidos a la seccion de usuarios,en esta </p>
+    
+</body>
+</html>
