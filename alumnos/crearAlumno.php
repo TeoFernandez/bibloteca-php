a7a307cb989ee33ccd8e40bb96f7206dc9909611
Teo Fernández <81999491+TeoFernandez@users.noreply.github.com>
2025-06-14 17:20:46 -0300

    Cambia el tipo de input para el DNI del alumno de número a texto en la página de creación de alumno y agrega un nuevo archivo CSS con estilos para mejorar la presentación.

--- alumnos/crearAlumno.php
+++ alumnos/crearAlumno.php
@@ -11,7 +11,7 @@
     <p>Bienvenido a la seccion de creacion de alumno</p>
     <center>
         <form action="POST">
-            DNI del alumno: <input type="number" name="dni_alumno"><br>
+            DNI del alumno: <input type="text" name="dni_alumno"><br>
             Nombre del alumno: <input type="text" name="nombre_alumno"><br>
             Apellido del alumno: <input type="text" name="apellido_alumno"><br>
             Carrera del alumno: <input type="number" name="carrera_usuario"><br>
