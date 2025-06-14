e7e41aa9346dbb618e23467793a89e55908aba41
Teo Fernández <81999491+TeoFernandez@users.noreply.github.com>
2025-06-14 17:30:41 -0300

    Corrige enlaces a hojas de estilo en las páginas de editar y eliminar alumno, y actualiza el tipo de input para el DNI en la página de eliminar alumno.

--- alumnos/eliminarAlumno.php
+++ alumnos/eliminarAlumno.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="styles/eliminarAlumno.css">
+    <link rel="stylesheet" href="styles/eliminar.css">
     <title>Eliminar alumno</title>
 </head>
 <body>
@@ -12,7 +12,7 @@
 
     <center>
         <form action="POST">
-            DNI del alumno: <input type="number" name="eliminar_usuario"><br>
+            DNI del alumno: <input type="text" name="eliminar_usuario"><br>
             <input type="submit" value="Eliminar Alumno">
         </form>
     </center>
