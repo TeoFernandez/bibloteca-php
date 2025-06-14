8e3ce0efaf232f96d19df2079de6fb8c61d321d2
Teo Fernández <81999491+TeoFernandez@users.noreply.github.com>
2025-06-14 17:51:31 -0300

    Elimina el archivo CSS antiguo y agrega un nuevo archivo con estilos mejorados para la página de edición de artículos.

--- articulos/editarArticulos.php
+++ articulos/editarArticulos.php
@@ -3,7 +3,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="styles/editarArticulos.css">
+    <link rel="stylesheet" href="editarArticulos.css">
+
 
     <title>editar Articulos</title>
 </head>
@@ -31,6 +32,7 @@
         <br><br><br>
 
         <button><a href="articulos.php">Volver</a></button>
+    </form>
 
 </body>
 </html>
