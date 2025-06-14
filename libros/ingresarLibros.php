968b54d6d22a463cb15ff17ed8a52b35ab121433
Nicolas Garcia <nicolasagustingarcia46@gmail.com>
2025-06-14 17:53:42 -0300

    Corrige la estructura del formulario en las páginas de eliminar e ingresar libros, eliminando títulos innecesarios y asegurando un cierre adecuado de etiquetas.

--- libros/ingresarLibros.php
+++ libros/ingresarLibros.php
@@ -45,11 +45,6 @@
         <button type="reset">Limpiar Formulario</button>
         <br><br><br>
         <button><a href="libros.php">Volver a la Sección de Libros</a></button>
-    </form>
-
-    <title>Document</title>
-</head>
-<body>
-    
+    </form>    
 </body>
 </html>
